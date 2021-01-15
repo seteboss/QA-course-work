2cf1811b15ac37213efad641962d82d7a69cd68e
Шапаренко Александр Александрович <shaparenko.aa@reliab.tech>
2021-01-16 00:18:51 +0300

    feat: configuration UITest

--- src/test/java/org/example/UITests.java
+++ src/test/java/org/example/UITests.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import org.example.pages.AccountPage;
+import org.example.pages.LoginPage;
+import org.example.util.ConfProperties;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class UITests {
+
+    public static WebDriver webDriver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.get(ConfProperties.getProperty("loginpage"));
+    }
+
+
+    @AfterClass
+    public static void tearDown() {
+        webDriver.quit();
+    }
+
 }
