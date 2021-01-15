1c390c943532118bb096480fa26161cfacc492fc
Шапаренко Александр Александрович <shaparenko.aa@reliab.tech>
2021-01-16 01:34:53 +0300

    feat: product test. Active pages: CatalogPage, ProductPage.

--- src/test/java/org/example/UITests.java
+++ src/test/java/org/example/UITests.java
@@ -3,22 +3,23 @@ package org.example;
 import org.example.pages.AccountPage;
 import org.example.pages.CatalogPage;
 import org.example.pages.LoginPage;
+import org.example.pages.ProductPage;
 import org.example.util.ConfProperties;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UITests {
 
     public static WebDriver webDriver;
     public static LoginPage loginPage;
     public static AccountPage accountPage;
     public static CatalogPage catalogPage;
+    public static ProductPage productPage;
 
 
     @BeforeClass
@@ -27,6 +28,8 @@ public class UITests {
         webDriver = new ChromeDriver();
         loginPage = new LoginPage(webDriver);
         accountPage = new AccountPage(webDriver);
+        catalogPage = new CatalogPage(webDriver);
+        productPage = new ProductPage(webDriver);
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.get(ConfProperties.getProperty("loginpage"));
@@ -34,41 +37,33 @@ public class UITests {
 
     @Test
     public void case1Login(){
-
         loginPage.inputLogin(ConfProperties.getProperty("login"));
-
         loginPage.inputPassword(ConfProperties.getProperty("password"));
-
         loginPage.clickCheckBox();
-
         loginPage.clickLoginBtn();
-
         String name = accountPage.getName();
-
         Assert.assertEquals(name, "sanek.7144241");
-
-
     }
 
     @Test
     public void case2StepInCatalog(){
-
         webDriver.get(ConfProperties.getProperty("bmwcatalogpage"));
-
         catalogPage.clickSortMenu();
-
         catalogPage.clickRatingSort();
-
         String productName = catalogPage.getProductName();
-
         Assert.assertEquals(productName, "Кроссовки BMW MMS RS-2K");
     }
 
-
+    @Test
+    public void case3StepInProduct(){
+        catalogPage.clickProductBtn();
+        String productName = productPage.getProductName();
+        Assert.assertEquals(productName, "Кроссовки BMW MMS RS-2K");
+    }
 
     @AfterClass
     public static void tearDown() {
-        catalogPage.clickAccountBtn();
+        productPage.clickAccountBtn();
         accountPage.exit();
         webDriver.quit();
     }
